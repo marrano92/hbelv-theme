774bc21b55663bd96be3066841c9d9f6dc5beecd
Andrea Marrano <andrea.marrano@drivek.com>
2019-08-27 15:16:52 +0200

    add search page

---
+++ parts/search.php
@@ -0,0 +1,24 @@
+<?php
+
+/**
+ * @var StdClass $hbelv;
+ */
+
+$rooms = $hbelv->items;
+?>
+<div class="content-search l-row">
+	<div class="card-deck">
+		<?php foreach ($rooms as $room){ ?>
+		<div class="card">
+			<img class="card-img-top" src="<?php echo $room->get_image('full'); ?>" alt="Card image cap">
+			<div class="card-body">
+				<h5 class="card-title"><?php echo $room->title; ?></h5>
+				<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+			</div>
+			<div class="card-footer">
+				<small class="text-muted">Last updated 3 mins ago</small>
+			</div>
+		</div>
+		<?php } ?>
+	</div>
+</div>
---
+++ single-roomsearch.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Template Name: Search
+ *
+ * @view search
+ * @package hotelilbelvedere
+ */
+
+get_header();
+?>
+
+	<main id="submodel-search" class="submodel-search page-main--full-viewport" role="main">
+		<section class="submodel-search__content">
+			<?php the_content(); ?>
+		</section>
+	</main>
+
+<?php get_footer(); ?>

21eab12e954027bc08a7ddd1debeb7a449b684ba
Andrea <andrea.marrao92@gmail.com>
2020-01-03 00:18:23 +0100

    building top bar styles

--- js/theme.js
+++ js/theme.js
@@ -7031,3 +7031,45 @@
     }, false);
   }
 })();
+document.getElementById("menu_icon").addEventListener("click", myFunction);
+
+function myFunction() {
+  var menu_text = document.getElementById("menu_text");
+  var menu_close_text = document.getElementById("menu_close_text");
+  var menu_big = document.getElementById("menu_big");
+  var div_menu = document.getElementById("div_menu");
+  var bar1 = document.getElementById("bar2");
+  var bar2 = document.getElementById("bar3");
+  var button = document.getElementById("button_book");
+
+  if (menu_text.classList.contains('flipOutX')) {
+    menu_close_text.classList.remove("flipInX");
+    menu_close_text.classList.add("flipOutX");
+    menu_text.classList.remove("flipOutX");
+    menu_close_text.classList.add("d-none");
+    menu_text.classList.add("flipInX");
+    menu_text.classList.remove("d-none");
+    menu_big.classList.remove("slideInDown");
+    menu_big.classList.add("slideOutUp");
+    div_menu.style.color = 'black';
+    bar1.style.background = 'black';
+    bar2.style.background = 'black';
+    button.classList.remove("white_button");
+    button.classList.add("blue_button");
+  } else {
+    menu_close_text.classList.remove("flipOutX");
+    menu_text.classList.remove("flipInX");
+    menu_text.classList.add("flipOutX");
+    menu_close_text.classList.add("flipInX");
+    menu_text.classList.add("d-none");
+    menu_close_text.classList.remove("d-none");
+    menu_big.classList.remove("slideOutUp");
+    menu_big.classList.add("slideInDown");
+    menu_big.classList.remove("d-none");
+    div_menu.style.color = 'white';
+    bar1.style.background = 'white';
+    bar2.style.background = 'white';
+    button.classList.remove("blue_button");
+    button.classList.add("white_button");
+  }
+}
--- parts/header/top-bar.php
+++ parts/header/top-bar.php
@@ -5,17 +5,76 @@
 	<div class="container-fluid height-top_bar" style="max-width: 1900px">
 		<div class="row height-top_bar pt-2 pb-2">
 			<div class="col-xl-5 h-100">
-				<h1>dsf</h1>
+				<div class="row">
+				<div class="col-xl-6 py-3 index-menu">
+					<div class="div_lang">
+						<a href="#0" class="number_telephone sm-link sm-link_text sm-link8" data-sm-link-text="IT">
+							<span class="sm-link__label">IT</span>
+						</a>
+					</div>
+					<div class="div_lang">
+						<a href="#0" class="number_telephone sm-link sm-link_text sm-link8" data-sm-link-text="EN">
+							<span class="sm-link__label">EN</span>
+						</a>
+					</div>
+					<div class="div_lang">
+						<a href="#0" class="number_telephone sm-link sm-link_text sm-link8" data-sm-link-text="DE">
+							<span class="sm-link__label">DE</span>
+						</a>
+					</div>
+				</div>
+				<div class="col-xl-6 py-3 index-menu">
+							<a href="#0" class="number_telephone sm-link sm-link_text sm-link8" data-sm-link-text="+39 423423432424">
+								<span class="sm-link__label">+39 423423432424</span>
+							</a>
+				</div>
+				</div>
 			</div>
 			<div class="col-xl-2 h-100 header__logo-container">
 				<img class="h-100 rounded mx-auto d-block" src="<?php echo get_template_directory_uri(); ?>/images/logo-nero.png">
 			</div>
-			<div class="col-xl-3 h-100 text-right are-vertically-aligned-center">
-				<a href="#" class="hvr-sweep-to-bottom px-4 py-6">Book now</a>
+			<div class="col-xl-3 h-100 text-right are-vertically-aligned-center py-3">
+				<a href="#" class="hvr-sweep-to-bottom blue_button px-5 py-3 button" id="button_book">Book now</a>
 			</div>
-			<div class="col-xl-2">
-				<h1>dsf</h1>
+			<div class="col-xl-2 text-center index-menu" id="div_menu">
+				<input type="checkbox" id="checkbox2" class="checkbox2 visuallyHidden">
+				<label for="checkbox2"  id="menu_icon" class="label_menu">
+					<div class="hamburger hamburger2 float-left" id="icon_hamburger">
+						<span class="bar bar1"></span>
+						<span class="bar bar2" id="bar2"></span>
+						<span class="bar bar3" id="bar3"></span>
+						<span class="bar bar4"></span>
+					</div>
+					<span id="menu_text" class="float-left menu_text animated">MENU</span>
+					<span id="menu_close_text" class="float-left menu_text animated d-none">CLOSE</span>
+				</label>
 			</div>
 		</div>
 	</div>
+	<main class="demo-inola animated d-none fast" id="menu_big">
+		<section class="content">
+			<nav class="menu menu--inola">
+				<a class="menu__item" href="#">
+					<span class="menu__item-name">Study With Us</span>
+					<span class="menu__item-label">Information on Courses</span>
+				</a>
+				<a class="menu__item" href="#">
+					<span class="menu__item-name">Research</span>
+					<span class="menu__item-label">Centers &amp; Groups</span>
+				</a>
+				<a class="menu__item" href="#">
+					<span class="menu__item-name">Alumni</span>
+					<span class="menu__item-label">Global Networks</span>
+				</a>
+				<a class="menu__item" href="#">
+					<span class="menu__item-name">International</span>
+					<span class="menu__item-label">Partnerships</span>
+				</a>
+				<a class="menu__item" href="#">
+					<span class="menu__item-name">Calendar</span>
+					<span class="menu__item-label">Important Dates</span>
+				</a>
+			</nav>
+		</section>
+	</main>
 </header>

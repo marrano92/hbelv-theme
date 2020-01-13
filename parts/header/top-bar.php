b3e2860b87a965530b61d0eba84e87e74bd2ff0d
andrea <andrea.marrano92@gmail.com>
2020-01-14 00:00:42 +0100

    Change topbar css

--- parts/header/top-bar.php
+++ parts/header/top-bar.php
@@ -4,41 +4,54 @@
 <header class="header-top-bar height-top_bar">
 	<div class="container-fluid height-top_bar" style="max-width: 1900px">
 		<div class="row height-top_bar pt-2 pb-2">
-			<div class="col-xl-5 h-100">
+			<div class="col-xl-5 col-md-5 d-sm-none d-none d-md-block h-100">
 				<div class="row">
-				<div class="col-xl-6 py-3 index-menu">
-					<div class="div_lang">
-						<a href="#0" class="number_telephone sm-link sm-link_text sm-link8" data-sm-link-text="IT">
-							<span class="sm-link__label">IT</span>
-						</a>
-					</div>
-					<div class="div_lang">
-						<a href="#0" class="number_telephone sm-link sm-link_text sm-link8" data-sm-link-text="EN">
-							<span class="sm-link__label">EN</span>
-						</a>
-					</div>
-					<div class="div_lang">
-						<a href="#0" class="number_telephone sm-link sm-link_text sm-link8" data-sm-link-text="DE">
-							<span class="sm-link__label">DE</span>
-						</a>
-					</div>
-				</div>
-				<div class="col-xl-6 py-3 index-menu">
-							<a href="#0" class="number_telephone sm-link sm-link_text sm-link8" data-sm-link-text="+39 423423432424">
-								<span class="sm-link__label">+39 423423432424</span>
+					<div class="col-xl-6 col-md-12 py-3 index-menu">
+						<div class="div_lang mr-md-1 ml-md-1">
+							<a href="#0" class="number_telephone sm-link sm-link_text sm-link8" id="languages1"
+							   data-sm-link-text="IT">
+								<span class="sm-link__label">IT</span>
 							</a>
+						</div>
+						<div class="div_lang mr-md-1 ml-md-1">
+							<a href="#0" class="number_telephone sm-link sm-link_text sm-link8" id="languages2"
+							   data-sm-link-text="EN">
+								<span class="sm-link__label">EN</span>
+							</a>
+						</div>
+						<div class="div_lang mr-md-1 ml-md-1">
+							<a href="#0" class="number_telephone sm-link sm-link_text sm-link8" id="languages3"
+							   data-sm-link-text="DE">
+								<span class="sm-link__label">DE</span>
+							</a>
+						</div>
+					</div>
+					<div class="col-xl-6 col-md-5 d-md-none d-lg-block py-3 index-menu">
+						<a href="#0" class="number_telephone sm-link sm-link_text sm-link8" id="number_telephone"
+						   data-sm-link-text="+39 423423432424">
+							<span class="sm-link__label">+39 423423432424</span>
+						</a>
+					</div>
 				</div>
-				</div>
 			</div>
-			<div class="col-xl-2 h-100 header__logo-container">
-				<img class="h-100 rounded mx-auto d-block" src="<?php echo get_template_directory_uri(); ?>/images/logo-nero.png">
+			<div class="d-md-none col-sm-4 h-100 d-none col-8 d-sm-block text-left text-md-right text-sm-left are-vertically-aligned-center py-3">
+				<a href="#" class="hvr-sweep-to-bottom blue_button px-md-5 py-md-3 py-sm-3 px-sm-4 button"
+				   id="button_book_mobile">Book now</a>
 			</div>
-			<div class="col-xl-3 h-100 text-right are-vertically-aligned-center py-3">
-				<a href="#" class="hvr-sweep-to-bottom blue_button px-5 py-3 button" id="button_book">Book now</a>
+			<div class="col-xl-2 col-md-2 col-sm-4 col-7 d-md-block h-100 header__logo-container index-menu">
+				<a href="<?php echo get_home_url(); ?>">
+					<img class="rounded pt-lg-0 mx-sm-auto py-2 mx-0 d-block" id="logo_image"
+						 src="<?php echo get_template_directory_uri(); ?>/images/logo-black.png"
+						 logo_black="<?php echo get_template_directory_uri(); ?>/images/logo-nero.png"
+						 logo_white="<?php echo get_template_directory_uri(); ?>/images/logo-white.png">
+				</a>
 			</div>
-			<div class="col-xl-2 text-center index-menu" id="div_menu">
+			<div class="col-xl-3 col-md-3 d-sm-none d-none d-md-block h-100 text-right are-vertically-aligned-center py-3">
+				<a href="#" class="hvr-sweep-to-bottom blue_button px-5 py-3 px-md-4 py-md-3 px-lg-5 py-lg-3 button" id="button_book">Book now</a>
+			</div>
+			<div class="col-xl-2 col-md-2 col-sm-4 col-5 d-md-block text-center index-menu" id="div_menu">
 				<input type="checkbox" id="checkbox2" class="checkbox2 visuallyHidden">
-				<label for="checkbox2"  id="menu_icon" class="label_menu">
+				<label for="checkbox2" id="menu_icon" class="label_menu mr-sm-0">
 					<div class="hamburger hamburger2 float-left" id="icon_hamburger">
 						<span class="bar bar1"></span>
 						<span class="bar bar2" id="bar2"></span>
--- src/js/custom-javascript.js
+++ src/js/custom-javascript.js
@@ -8,6 +8,13 @@ function myFunction() {
 	var bar1 = document.getElementById("bar2");
 	var bar2 = document.getElementById("bar3");
 	var button = document.getElementById("button_book");
+	var button_mobile = document.getElementById("button_book_mobile");
+	var number = document.getElementById("number_telephone");
+	var languages1 = document.getElementById("languages1");
+	var languages2 = document.getElementById("languages2");
+	var languages3 = document.getElementById("languages3");
+	var logo = document.getElementById("logo_image");
+
 
 	if (menu_text.classList.contains('flipOutX')) {
 		menu_close_text.classList.remove("flipInX");
@@ -23,6 +30,13 @@ function myFunction() {
 		bar2.style.background = 'black';
 		button.classList.remove("white_button");
 		button.classList.add("blue_button");
+		button_mobile.classList.add("blue_button");
+		button_mobile.classList.add("blue_button");
+		number.classList.remove("text-white");
+		languages1.classList.remove("text-white");
+		languages2.classList.remove("text-white");
+		languages3.classList.remove("text-white");
+		logo.setAttribute('src', logo.getAttribute("logo_black"));
 	} else {
 		menu_close_text.classList.remove("flipOutX");
 		menu_text.classList.remove("flipInX");
@@ -38,5 +52,13 @@ function myFunction() {
 		bar2.style.background = 'white';
 		button.classList.remove("blue_button");
 		button.classList.add("white_button");
+		button_mobile.classList.remove("blue_button");
+		button_mobile.classList.add("white_button");
+		number.classList.add("text-white");
+		languages1.classList.add("text-white");
+		languages2.classList.add("text-white");
+		languages3.classList.add("text-white");
+		logo.setAttribute('src', logo.getAttribute("logo_white"));
+
 	}
 }
